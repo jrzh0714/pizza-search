cdeaf1f86b671ebff4b8105a6b495d36a4fa03c3
Jerry Zheng <jiaruizheng0714@gmail.com>
2015-12-16 22:56:34 +0000

    The Ultimate Pizza Search Engine

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+function loadWindowSystem(){
+     var theObject = document.getElementById("results");
+    var theText = theObject.innerHTML;
+    theText = theText.replace(/"undefined"/g, "No Website!");
+    theText = theText.replace(/undefined/g, "No Website!");
+    console.log(theText);
+    theObject.innerHTML = theText;
+}
 $( document ).ready(function() {
     var client_id = "3S04IG4W3DQJX0XSATONM4JL0B1V5UPTLSEGZABLGEE45IRV";
     var client_secret = "TU44KTVPTWYEHKJPLXJ1CHKOXQ24COKDXPPKGFH2ZZA34NWD";
+    
   $("#submit").click(function(event) {
       var address = $("#address").val();
       var food = $("#food").val();
@@ -12,11 +21,15 @@ $( document ).ready(function() {
         "&query=" +food,
     function(data) {
     for (var i=0;i< data["response"]["venues"].length;i++){
-           $("#results").append("<li>" + data["response"]["venues"][i]["name"] + "<br>" +" Phone Number: "+ data["response"]["venues"][i]["contact"]["formattedPhone"] + "<br>"+ "<a href=" +"'" +data["response"]["venues"][i]["url"]+"'" + ">website</a>" +"</li><br>") 
-           console.log(data["response"] );
+           $("#results").append("<li>" + data["response"]["venues"][i]["name"] + "<br>" +" Phone Number: "+ data["response"]["venues"][i]["contact"]["formattedPhone"] + "<br>"+ "<a href=" +"'" +data["response"]["venues"][i]["url"]+"'" + ">"+data["response"]["venues"][i]["url"]+"</a>"+"<br>"+"Address: "+data["response"]["venues"][i]["location"]["formattedAddress"][0]+", "+data["response"]["venues"][i]["location"]["formattedAddress"][1]+", "+data["response"]["venues"][i]["location"]["formattedAddress"][2]+"</li><br>") 
+           
         
     };
+    console.log(data["response"]);
+   loadWindowSystem();
+
     });
         });
+       
     });
 ;
